a7f3c084f7b18999f9a43c59261f2d14d546b7ad
Tamir Bahar <tamir.bahar@gmail.com>
2025-05-31 13:11:25 +0300

    Work towards detecting read/write xrefs

---
+++ scripts/generate-parsers.ts
@@ -0,0 +1,69 @@
+import * as fs from "node:fs";
+import { mkdir } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+/**
+ * The `generate-parsers` script copies or builds the relevant tree-sitter
+ * parsers in to the `./parsers` directory.
+ *
+ * To add a new parsers, add it's package name to the `parsersToBuild` array.
+ * @module
+ */
+import { $ } from "bun";
+
+function isString(value: unknown): value is string {
+  return typeof value === "string" || value instanceof String;
+}
+
+type Location = { package: string; name: string };
+function asLocation(entry: string | Location): Location {
+  if (isString(entry)) {
+    return { package: entry, name: entry };
+  }
+  return entry;
+}
+
+/**
+ * The parsers to include
+ */
+const parsersToBuild: (Location | string)[] = ["tree-sitter-python"];
+
+function locatePrebuiltWasm(location: Location): string {
+  return fileURLToPath(
+    import.meta.resolve(`${location.package}/${location.name}.wasm`),
+  );
+}
+
+function hasPrebuiltWasm(location: Location): boolean {
+  try {
+    locatePrebuiltWasm(location);
+  } catch {
+    return false;
+  }
+  return true;
+}
+
+await mkdir("./parsers");
+
+for (const location of parsersToBuild.map(asLocation)) {
+  const targetWasmPath = `./parsers/${location.name}.wasm`;
+  if (await Bun.file(targetWasmPath).exists()) {
+    console.log(`${location.name}: .wasm found, skipping copy.`);
+  } else if (hasPrebuiltWasm(location)) {
+    console.log(`${location.name}: copying .wasm`);
+    fs.copyFileSync(locatePrebuiltWasm(location), targetWasmPath);
+  } else {
+    console.log(`${location.name}: building .wasm`);
+    await $`bun x --bun tree-sitter build --wasm -o ${targetWasmPath} ./node_modules/${location.package}/`;
+  }
+
+  await $`git add ${targetWasmPath}`;
+}
+
+const treeSitterPath = "./parsers/tree-sitter.wasm";
+if (!(await Bun.file(treeSitterPath).exists())) {
+  const treeSitter = Bun.file(
+    "./node_modules/web-tree-sitter/tree-sitter.wasm",
+  );
+  await Bun.write(treeSitterPath, treeSitter);
+  await $`git add ${treeSitterPath}`;
+}
---
+++ test/python.test.ts
@@ -0,0 +1,90 @@
+import path from "node:path";
+import { expect, test } from "vitest";
+import { Language, type Node, Parser, type Tree } from "web-tree-sitter";
+async function loadParser(): Promise<Parser> {
+  await Parser.init();
+  const wasmPath = path.join(
+    process.cwd(),
+    "./parsers/tree-sitter-python.wasm",
+  );
+  const language = await Language.load(wasmPath);
+  const parser = new Parser();
+  parser.setLanguage(language);
+  return parser;
+}
+
+const parser = await loadParser();
+
+function parseSample(code: string, needle: string): { tree: Tree; node: Node } {
+  const tree = parser.parse(code);
+  if (!tree) {
+    throw new Error("Failed parsing code.");
+  }
+  const index = code.indexOf(needle);
+  if (index === -1) {
+    throw new Error("Needle not found in code.");
+  }
+  const node = tree.rootNode.descendantForIndex(index);
+  if (!node) {
+    throw new Error("No matching node found.");
+  }
+
+  return { tree, node };
+}
+
+test("Basic Python Parsing", async () => {
+  const tree = parser.parse("a = 3");
+  expect(tree).toBeTruthy();
+});
+
+test("Find variable in sample", async () => {
+  const { node } = parseSample("a = 2", "a");
+  expect(node.type).toEqual("identifier");
+});
+
+function isWrite(target: Node): boolean {
+  if (target.type !== "identifier") {
+    throw new Error("Target node must be an identifier!");
+  }
+  let node = target;
+  let parent = node.parent;
+  for (; parent; node = parent, parent = node.parent) {
+    switch (parent.type) {
+      case "assignment": {
+        if (parent.childForFieldName("left")?.id === node.id) {
+          return true;
+        }
+        break;
+      }
+      case "attribute": {
+        if (parent.childForFieldName("object")?.id === node.id) {
+          return false;
+        }
+        break;
+      }
+      case "subscript": {
+        if (parent.childForFieldName("subscript")?.id === node.id) {
+          return false;
+        }
+        break;
+      }
+    }
+  }
+  return false;
+}
+
+test.each([
+  ["a = 2", true],
+  ["b = a", false],
+  ["x = a = 3", true],
+  ["a.x = 2", false],
+  ["s[a] = 3", false],
+  ["a, b = l", true],
+  ["x.a = 1", true],
+  ["x[2].a = 1", true],
+  ["a[1] = 2", true],
+  ["f(a)", false],
+])("is a written to in %s ?", (code, expected) => {
+  const { node } = parseSample(code, "a");
+  expect(isWrite(node)).toEqual(expected);
+});
