03463261ff86330768935046759d4d6b20facc75
Tamir Bahar <tamir.bahar@gmail.com>
2025-06-02 18:27:00 +0300

    More moving!

--- test/python.test.ts
+++ test/python.test.ts
@@ -1,6 +1,8 @@
 import path from "node:path";
-import { expect, test } from "vitest";
-import { Language, type Node, Parser, type Tree } from "web-tree-sitter";
+import {expect, test} from "vitest";
+import {Language, type Node, Parser, type Tree} from "web-tree-sitter";
+import {isWrite} from "../src/classifier";
+
 async function loadParser(): Promise<Parser> {
   // await Parser.init();
   const wasmPath = path.join(
@@ -41,37 +43,6 @@ test("Find variable in sample", async () => {
   const { node } = parseSample("a = 2", "a");
   expect(node.type).toEqual("identifier");
 });
-
-function isWrite(target: Node): boolean {
-  if (target.type !== "identifier") {
-    throw new Error("Target node must be an identifier!");
-  }
-  let node = target;
-  let parent = node.parent;
-  for (; parent; node = parent, parent = node.parent) {
-    switch (parent.type) {
-      case "assignment": {
-        if (parent.childForFieldName("left")?.id === node.id) {
-          return true;
-        }
-        break;
-      }
-      case "attribute": {
-        if (parent.childForFieldName("object")?.id === node.id) {
-          return false;
-        }
-        break;
-      }
-      case "subscript": {
-        if (parent.childForFieldName("subscript")?.id === node.id) {
-          return false;
-        }
-        break;
-      }
-    }
-  }
-  return false;
-}
 
 test.each([
   ["a = 2", true],
